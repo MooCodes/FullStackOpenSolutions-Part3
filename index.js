2bf053d3b92d815787ca64a170da351f4d9dfbb0
MooCodes <60076255+MooCodes@users.noreply.github.com>
2023-11-22 10:43:07 -0600

    3.10 phonebook backend step10

--- index.js
+++ index.js
@@ -97,6 +97,7 @@ app.delete("/api/persons/:id", (req, res) => {
   res.status(204).end();
 });
 
+// part 3.10
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
