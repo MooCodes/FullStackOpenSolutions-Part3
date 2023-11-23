05033b87c6d7f07b9f182b14bda43e4087c3a0a7
MooCodes <60076255+MooCodes@users.noreply.github.com>
2023-11-23 10:56:22 -0600

    uibuild

--- index.js
+++ index.js
@@ -14,29 +14,6 @@ app.use(
 app.use(cors());
 app.use(express.static("dist"));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -78,14 +55,6 @@ app.post("/api/persons", (req, res) => {
       error: "number is missing",
     });
   }
-
-  // const alreadyExists = persons.find((person) => person.name === body.name);
-
-  // if (alreadyExists) {
-  //   return res.status(400).json({
-  //     error: "name must be unique",
-  //   });
-  // }
 
   const person = new Person({
     name: body.name,
